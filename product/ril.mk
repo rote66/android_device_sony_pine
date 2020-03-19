53a68b28a1dc55c06cb9e5accb2417c272b0d620
rote66 <pontifex23872388@gmail.com>
2020-03-20 00:56:03 +0800

    revert ril.mk

--- product/ril.mk
+++ product/ril.mk
@@ -1,7 +1,6 @@
 # RIL
 PRODUCT_PACKAGES += \
-    libprotobuf-cpp-full \
-    libccci_util 
+    libprotobuf-cpp-full 
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.kernel.android.checkjni=0 \
@@ -10,19 +9,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.android.mobiledata=false \
     ro.kernel.android.checkjni=0
 
-# Telephony
-SIM_COUNT := 2
-PRODUCT_PROPERTY_OVERRIDES += ro.telephony.sim.count=$(SIM_COUNT)
-PRODUCT_PROPERTY_OVERRIDES += persist.radio.default.sim=0
-PRODUCT_PROPERTY_OVERRIDES += persist.radio.multisim.config=dsds
-
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/spn-conf.xml:system/etc/spn-conf.xml 
 
-# RIL
-PRODUCT_PACKAGES += \
-    libccci_util \
-    librilmtk \
-    mtkrild \
-    rilproxy \
-    libril
